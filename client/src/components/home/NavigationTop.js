d688237cbeabac8e24bf7b45956a08850852998a
remisany <remi.d.sany@gamil.com>
2023-05-02 16:57:59 -0400

    responsive

--- client/src/components/home/NavigationTop.js
+++ client/src/components/home/NavigationTop.js
@@ -24,3 +24,45 @@ const NavigationTop = () => {
 }
 
 export default NavigationTop
+
+/*
+
+    let first = true
+
+    const getScale = () => {
+        const width = window.innerWidth
+        const height = window.document.documentElement.clientHeight
+
+        switch (true) {
+            case (width >= 1200):
+                break
+            case (width >= 1024):
+                break
+            case (width >= 768 && width < height):
+                break
+            case (width >= 768 && width > height):
+                break
+            case (width >= 480):
+                break
+            default:
+                break
+        }
+    }
+
+    const handleResize = () => {
+        if (first) {
+            first = false
+        } else {
+            window.scrollTo(0, 0);
+            window.location.reload()
+        }
+    }
+
+    useEffect(() => {
+        const resizeObserver = new ResizeObserver(handleResize)
+        resizeObserver.observe(document.body)
+
+        getScale()
+    },[])
+
+ */
