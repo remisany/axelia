02f6cb76ee2ad2add32380a25b028eae1230208c
remisany <remi.d.sany@gamil.com>
2023-06-04 17:17:18 -0400

    responsive homePage

--- client/src/components/home/Navigation.js
+++ client/src/components/home/Navigation.js
@@ -25,32 +25,33 @@ const Navigation = () => {
 
         switch (true) {
             case width < 407:
-                multiple = 2;
-                break;
+                multiple = 2
+                break
             case width >= 407 && width < 480:
-                multiple = 2;
-                break;
+                multiple = 2
+                break
             case width >= 480 && width < 768 && window.matchMedia("(orientation: landscape)").matches:
-                multiple = 1;
-                break;
+                multiple = 1
+                break
             case width >= 480 && width < 768 && window.matchMedia("(orientation: portrait)").matches:
-                multiple = 2;
-                break;
+                multiple = 2
+                break
             case width >= 768 && width < 1024:
-                multiple = 1.2;
-                break;
+                multiple = 1.2
+                break
             case width >= 1024 && width < 1200:
-                multiple = 1;
-                break;
+                multiple = 1
+                break
             default:
-                multiple = 1;
-                break;
+                multiple = 1
+                break
         }
 
         return multiple
     }
 
-    const height = (getMultiple() * 4409) / (17067 / window.innerWidth);
+    const width = (getMultiple() * 17067) / (17067 / window.innerWidth) * 0.33
+    const height = (getMultiple() * 4409) / (17067 / window.innerWidth)
     const top = (0.5 * window.innerHeight) - (height / 2)
 
     return (
@@ -61,10 +62,10 @@ const Navigation = () => {
 
             <div>
                 <CSSTransition classNames='nnp-transition' in={animate} timeout={1000} unmountOnExit>
-                    <div className='nnp-title' style={{height: height + 'px', top: top + 'px'}}>{url && url.replace("/", ".")}</div>
+                    <div className='nnp-title' style={{height: height + 'px', top: top + 'px'}}>{url && url.replace("/", "")}</div>
                 </CSSTransition>
 
-                <div className={`nnp-clickable ${url && 'active'}`} onClick={onClick}></div>
+                <div className={`nnp-clickable ${url && 'active'}`} style={{height: height + 'px', width: width + 'px'}} onClick={onClick}></div>
                 <img className='nnp-stencil' src={Stencil} alt='camera icon'/>
             </div>
         </div>
